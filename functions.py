c1e24fac11de6beac7dcdd77aa17cceef0278d91
sean15535 <sean15535@gmail.com>
2024-03-09 12:47:45 +0100

    fixed the add item function

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 shopping_list = []
+
 def greetings():
     print("\nShopping List Manager")
     print("1. View Shopping List")
@@ -6,18 +7,31 @@ def greetings():
     print("3. Remove Item from Shopping List")
     print("4. Clear Shopping List")
     print("5. Exit")
+
 def view_shopping_list():
     if not shopping_list:
-        return "Your shopping list is empty."
+        print("Your shopping list is empty.")
+        return
     print("Your Shopping List:")
     for index, item in enumerate(shopping_list, start=1):
         print(f"{index}. {item}")
-def add_item():
+
+def remove_item():
     if not shopping_list:
         return "Your shopping list is empty."
     index = int(input("Enter the index of the item to remove: "))
-    if 1 <= index <= len(shopping_list):
-        removed_item = shopping_list.pop(index - 1)
-        return f"'{removed_item}' removed from your shopping list."
-    else:
+    try:
+        if 1 <= index <= len(shopping_list):
+            removed_item = shopping_list.pop(index - 1)
+            print(f"'{removed_item}' removed from your shopping list.")
+    except ValueError:
         print("Invalid index.")
+
+def add_item():
+        item = input("Enter the item you want to add: ")
+        shopping_list.append(item)
+        return f"'{item}' added to your shopping list."
+
+def clear_list():
+    shopping_list.clear()
+    print("Shopping list cleared.")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from functions import greetings, view_shopping_list, add_item
+from functions import greetings, view_shopping_list, add_item, remove_item, clear_list
 print("Welcome to Shoprite Shopping list Manager ")
 while True:
     greetings()
@@ -7,14 +7,11 @@ while True:
     if choice == '1':
         view_shopping_list()
     elif choice == '2':
-        item = input("Enter the item you want to add: ")
-        shopping_list.append(item)
-        print(f"'{item}' added to your shopping list.")
-    elif choice == '3':
         add_item()
+    elif choice == '3':
+        remove_item()
     elif choice == '4':
-        shopping_list.clear()
-        print("Shopping list cleared.")
+        clear_list()
     elif choice == '5':
         print("Thank you for using the Shoprite Shopping List Manager. Goodbye!")
         break
