5e592cdddc599cd3f360033cc49960d8570001c1
sean15535 <sean15535@gmail.com>
2024-03-09 12:21:52 +0100

    adding functions to the code

--- functions.py
+++ functions.py
@@ -1,1 +1,23 @@
 shopping_list = []
+def greetings():
+    print("\nShopping List Manager")
+    print("1. View Shopping List")
+    print("2. Add Item to Shopping List")
+    print("3. Remove Item from Shopping List")
+    print("4. Clear Shopping List")
+    print("5. Exit")
+def view_shopping_list():
+    if not shopping_list:
+        return "Your shopping list is empty."
+    print("Your Shopping List:")
+    for index, item in enumerate(shopping_list, start=1):
+        print(f"{index}. {item}")
+def add_item():
+    if not shopping_list:
+        return "Your shopping list is empty."
+    index = int(input("Enter the index of the item to remove: "))
+    if 1 <= index <= len(shopping_list):
+        removed_item = shopping_list.pop(index - 1)
+        return f"'{removed_item}' removed from your shopping list."
+    else:
+        print("Invalid index.")
--- main.py
+++ main.py
@@ -1,36 +1,17 @@
-
+from functions import greetings, view_shopping_list, add_item
 print("Welcome to Shoprite Shopping list Manager ")
 while True:
-    print("\nShopping List Manager")
-    print("1. View Shopping List")
-    print("2. Add Item to Shopping List")
-    print("3. Remove Item from Shopping List")
-    print("4. Clear Shopping List")
-    print("5. Exit")
+    greetings()
 
     choice = input("Enter your choice: ")
-
     if choice == '1':
-        if not shopping_list:
-            print("Your shopping list is empty.")
-        else:
-            print("Your Shopping List:")
-            for index, item in enumerate(shopping_list, start=1):
-                print(f"{index}. {item}")
+        view_shopping_list()
     elif choice == '2':
         item = input("Enter the item you want to add: ")
         shopping_list.append(item)
         print(f"'{item}' added to your shopping list.")
     elif choice == '3':
-        if not shopping_list:
-            print("Your shopping list is empty.")
-        else:
-            index = int(input("Enter the index of the item to remove: "))
-            if 1 <= index <= len(shopping_list):
-                removed_item = shopping_list.pop(index - 1)
-                print(f"'{removed_item}' removed from your shopping list.")
-            else:
-                print("Invalid index.")
+        add_item()
     elif choice == '4':
         shopping_list.clear()
         print("Shopping list cleared.")
